ce99c094ac7756ed7b452dc7f5b516e3aeb38347
Pierre Guillaume <pierre.guillaume@epita.fr>
2020-06-18 12:12:02 +0200

    to binary string function done

--- src/lzw.py
+++ src/lzw.py
@@ -2,12 +2,33 @@
 
 from argparse import ArgumentParser
 
+def build_dico():
+    # build the default dictionary
+    pass
+
+def to_bin(value, n):
+    '''
+    Convert the value into binary string representation
+
+    :param value: value to convert
+    :param n: number of bit(s) for convertion
+
+    :type value: int
+    :type n: int
+
+    :return: string binary representation
+    :rtype: string
+    '''
+    tmp = bin(value).split('b')[1]
+    return '0' * (n - len(tmp)) + tmp
+
 def _build_arg_list():
     '''
     Build the list of arguments
     '''
     arg_parser = ArgumentParser(description="LZW compression")
     arg_parser.add_argument('-c', action="store_true", help="Launch the compression")
+    arg_parser.add_argument('-u', action="store_true", help="Launch the decompression")
     arg_parser.add_argument('-p', help="Specify the path of the text file")
     return arg_parser.parse_args()
 
--- src/test_lzw.py
+++ src/test_lzw.py
@@ -2,4 +2,32 @@
 
 import pytest
 
-# TODO -> add some tests
+from lzw import to_bin
+
+########################
+# Test: to_bin         #
+########################
+
+def test_to_bin_1():
+    ref = '0000010011'
+    v = 19
+    n = 10
+    assert to_bin(v, n) == ref
+
+def test_to_bin_2():
+    ref = '1001'
+    v = 9
+    n = 4
+    assert to_bin(v, n) == ref
+
+def test_to_bin_3():
+    ref = '01001'
+    v = 9
+    n = 5
+    assert to_bin(v, n) == ref
+
+def test_to_bin_4():
+    ref = '0000'
+    v = 0
+    n = 4
+    assert to_bin(v, n) == ref
